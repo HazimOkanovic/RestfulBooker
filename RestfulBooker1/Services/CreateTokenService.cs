e6b28f4087b7ccea9249c2f578fffa8fb71610bf
Hazim Okanovic <62857146+HazimOkanovic@users.noreply.github.com>
2023-01-24 14:26:00 +0100

    Feature/create token (#2)
    * added create token service class
    
    * added request and response models
    
    * added a class for tests and edited config helper class
    
    * update

---
+++ RestfulBooker1/Services/CreateTokenService.cs
@@ -0,0 +1,42 @@
+using System;
+using RestfulBooker1.Models.CreateToken.Request;
+using RestfulBooker1.Models.CreateToken.Response;
+using RestSharp;
+
+namespace RestfulBooker1.Services
+{
+    public class CreateTokenService : BaseService
+    {
+        public CreateTokenService(string baseUrl) : base(baseUrl)
+        {
+            
+        }
+        
+        public RestResponse<CreateTokenResponse> CreateToken(string username, string password)
+        {
+            var rsp = PostAsync<CreateTokenRequest, CreateTokenResponse>(
+                "/auth",
+                new CreateTokenRequest(username, password)
+            ).Result;
+            
+            Console.WriteLine("------");
+            Console.WriteLine("Create token content: " + rsp.Content);
+            Console.WriteLine("------");
+            return rsp;
+        }
+        
+        public RestResponse<CreateTokenErrorResponse> CreateTokenError(string username, string password)
+        {
+            var rsp = PostAsync<CreateTokenRequest, CreateTokenErrorResponse>(
+                "/auth",
+                new CreateTokenRequest(username, password)
+            ).Result;
+            
+            Console.WriteLine("Login request content:");
+            Console.WriteLine("------");
+            Console.WriteLine("Login response content: " + rsp.Content);
+            Console.WriteLine("------");
+            return rsp;
+        }
+    }
+}
