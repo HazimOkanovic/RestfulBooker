0de9befa51f7a876d0a69aa48e9a7a11125f9b25
Hazim Okanovic <hazimokanovic@live.com>
2023-01-24 14:22:14 +0100

    added a class for tests and edited config helper class

--- RestfulBooker1/Constants.cs
+++ RestfulBooker1/Constants.cs
@@ -2,7 +2,11 @@ namespace RestfulBooker1
 {
     public class Constants
     {
+        public const string OkStatus = "OK";
         public const string ValidUsername = "admin";
         public const string ValidPassword = "password123";
+        public const string BadCredentials = "Bad credentials";
+        public const string WrongUsername = "admin123";
+        public const string WrongPassword = "password";
     }
 }
