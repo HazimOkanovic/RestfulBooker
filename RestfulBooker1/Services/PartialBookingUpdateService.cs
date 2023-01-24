7814a97c40621b99a0f7c5a2bff2e5b4f2deedf6
Hazim Okanovic <hazimokanovic@live.com>
2023-01-24 16:45:25 +0100

    added tests for partial update and updates the service and base class

--- RestfulBooker1/Services/PartialBookingUpdateService.cs
+++ RestfulBooker1/Services/PartialBookingUpdateService.cs
@@ -16,7 +16,7 @@ namespace RestfulBooker1.Services
         
         public RestResponse<PartialBookingUpdateResponse> FirstAndLastNameUpdate(string firstName, string lastName)
         {
-            var rsp = PutAsync<FirstAndLastNameUpdateRequest, PartialBookingUpdateResponse>(
+            var rsp = PatchAsync<FirstAndLastNameUpdateRequest, PartialBookingUpdateResponse>(
                 "/booking/" + CreateBookingHelper.BookingId,
                 new FirstAndLastNameUpdateRequest(firstName, lastName), CreateTokenHelper.Token
             ).Result;
@@ -29,7 +29,7 @@ namespace RestfulBooker1.Services
         
         public RestResponse<PartialBookingUpdateResponse> PriceDepositAndNeedsUpdate(int price, bool paidDeposit, string additionalNeeds)
         {
-            var rsp = PutAsync<PriceDepositAndAdditionalNeedsRequest, PartialBookingUpdateResponse>(
+            var rsp = PatchAsync<PriceDepositAndAdditionalNeedsRequest, PartialBookingUpdateResponse>(
                 "/booking/" + CreateBookingHelper.BookingId,
                 new PriceDepositAndAdditionalNeedsRequest(price, paidDeposit, additionalNeeds), CreateTokenHelper.Token
             ).Result;
@@ -42,20 +42,7 @@ namespace RestfulBooker1.Services
         
         public RestResponse<PartialBookingUpdateResponse> PartialUpdateBookingNoAuth(string firstName, string lastName)
         {
-            var rsp = PutAsyncWithoutAuth<FirstAndLastNameUpdateRequest, PartialBookingUpdateResponse>(
-                "/booking/" + CreateBookingHelper.BookingId,
-                new FirstAndLastNameUpdateRequest(firstName, lastName), CreateTokenHelper.Token
-            ).Result;
-            
-            Console.WriteLine("------");
-            Console.WriteLine("Update booking content: " + rsp.Content);
-            Console.WriteLine("------");
-            return rsp;
-        }
-        
-        public RestResponse<PartialBookingUpdateResponse> ErrorPartialUpdateBooking(string firstName, string lastName)
-        {
-            var rsp = PutAsync<FirstAndLastNameUpdateRequest, PartialBookingUpdateResponse>(
+            var rsp = PatchAsyncWithoutAuth<FirstAndLastNameUpdateRequest, PartialBookingUpdateResponse>(
                 "/booking/" + CreateBookingHelper.BookingId,
                 new FirstAndLastNameUpdateRequest(firstName, lastName), CreateTokenHelper.Token
             ).Result;
