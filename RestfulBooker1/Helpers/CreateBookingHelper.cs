fa1be877a1320767d5ddc95d56da798347e42105
Hazim Okanovic <hazimokanovic@live.com>
2023-01-24 14:31:06 +0100

    added create booking service and edited create booking helper

--- RestfulBooker1/Helpers/CreateBookingHelper.cs
+++ RestfulBooker1/Helpers/CreateBookingHelper.cs
@@ -2,6 +2,23 @@ namespace RestfulBooker1.Helpers
 {
     public class CreateBookingHelper
     {
+        public static string BookingId { get; set; }
         
+        public string CreateBooking()
+        {
+            CreateBookingService service = new CreateBookingService(ConfigHelper.WEB_API_URL);
+
+            RestResponse<CreateBookingResponse> response = service.CreateBooking(Constants.FirstName,
+                Constants.LastName, 200, true, new BookingDatesRequest(DateTime.Today.Date.ToString("MM/dd/yyyy"),
+                    DateTime.Today.Date.ToString("MM/dd/yyyy")), "nothing");
+
+            int bookingId = response.Data.BookingId;
+
+            string id = bookingId.ToString();
+            
+            BookingId = id;
+
+            return id;
+        }
     }
 }
